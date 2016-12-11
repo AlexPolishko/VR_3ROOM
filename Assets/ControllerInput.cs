035a2623e4a6a2bc2d9749e01089dc07ca300d19
Alex <Alexanderrff@gmail.com>
2016-12-11 21:57:03 +0200

    Camera controlled by DayDream

---
+++ Assets/ControllerInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerInput : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject controllerPivot;
+	
+	// Update is called once per frame
+	void Update () {
+		if (GvrController.State != GvrConnectionState.Connected) {
+			controllerPivot.SetActive(false);
+		}
+		controllerPivot.SetActive(true);
+		controllerPivot.transform.rotation = GvrController.Orientation;
+	}
+}
